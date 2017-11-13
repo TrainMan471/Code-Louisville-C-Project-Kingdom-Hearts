6cf6507bab3dd1e0e02a0ec0ea0c26f9c45b0f8d
Mike Brown <brownmichael486@gmail.com>
2017-11-13 15:48:19 -0500

    Added Data Access Layer

---
+++ KingdomHeartsApplication/DAL/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KingdomHeartsApplication;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace KingdomHeartsApplication.DAL
+{
+    public class DB : DbContext
+    {
+
+    }
+}
